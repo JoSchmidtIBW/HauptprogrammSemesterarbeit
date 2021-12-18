06d5ac4a6c806694d1f184604618ca82fa6971a5
John <flnc85@hotmail.de>
2021-12-18 23:17:46 +0100

    Sprache muss gewählt sein für weiter

--- src/main/java/GUI/Sprache.java
+++ src/main/java/GUI/Sprache.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -14,6 +15,8 @@ import javafx.stage.Stage;
  */
 public class Sprache {
 
+    //private static String[] sprachen = {"Deutsch", "English", "France","Turky","Chinesisch"};
+   //String[] sprachen = {"Deutsch", "English", "France","Turky","Chinesisch"};
     //Deutsch
     //serbisch
     //bosnisch
@@ -28,30 +31,53 @@ public class Sprache {
         //TextField txfLoginBenutzername = new TextField();
         //Button bLoginBenutzerNameOK = new Button("LoginBenutzerNameOK");
 
+       // String[] sprachen = {"Deutsch", "English", "France","Turky","Chinesisch"};
+
+        //ComboBox comboBox1 = new ComboBox(sprachen);
         ComboBox comboBox1 = new ComboBox();
 
         comboBox1.getItems().add("Deutsch");
-        comboBox1.getItems().add("Serbisch");
+        comboBox1.getItems().add("Italienisch");
+        comboBox1.getItems().add("serbo-koratisch");//serbo-koratisch
         comboBox1.getItems().add("Bosnisch");
-        comboBox1.getItems().add("Albanisch");
+        comboBox1.getItems().add("albanisch mazedonisch Shkumbin");//albanisch mazedonisch Shkumbin
         comboBox1.getItems().add("Türkisch");
 
-        Label lZeigeSprachAuswahl = new Label("No country selected yet!");
+        Label lZeigeSprachAuswahl = new Label("No language selected yet!");
 
+        System.out.println("blablbla  "+comboBox1.getValue());
 
         /*Event handler for getting selected country*/
+
+
+
+
         EventHandler<ActionEvent> event =
                 new EventHandler<ActionEvent>() {
                     public void handle(ActionEvent e)
                     {
-                        lZeigeSprachAuswahl.setText("You selected \"" + comboBox1.getValue() + "\" country");
+                        lZeigeSprachAuswahl.setTextFill(Color.rgb(21, 117, 84));
+                        lZeigeSprachAuswahl.setText("You selected \"" + comboBox1.getValue());
                     }
                 };
         comboBox1.setOnAction(event);
 
 
+
+
+
+        //Label lzeigeSprache
         Button buttonHauptGUI = new Button("Go to HauptGui wenn Sprache gewählt oder standart deutsch");
-        buttonHauptGUI.setOnAction(e -> stage.setScene(HauptGUIScene.createGetHauptScene(stage)));
+        buttonHauptGUI.setOnAction(e -> {
+            if (comboBox1.getValue() == null) {
+                System.out.println("Sprache ist null");
+                lZeigeSprachAuswahl.setTextFill(Color.rgb(210, 39, 30));
+            } else {
+                System.out.println("Sprache ist gewählt");
+                stage.setScene(HauptGUIScene.createGetHauptScene(stage));
+            }
+        });
+
 
         VBox layoutV1 = new VBox(1);
         HBox layoutHSprachAuswahl = new HBox(1);
