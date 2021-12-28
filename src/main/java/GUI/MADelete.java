e2c01f036e906f908f74cb151f0bf3ac574f57f5
John <flnc85@hotmail.de>
2021-12-28 21:18:35 +0100

    naja, bisschen Oberfläche versucht zu machen

--- src/main/java/GUI/MADelete.java
+++ src/main/java/GUI/MADelete.java
@@ -15,23 +15,31 @@ import javafx.stage.Stage;
  */
 public class MADelete {
 
+    public static String getEingabeMANummerDelete() {
+        return eingabeMANummerDelete;
+    }
+
+    public static void setEingabeMANummerDelete(String eingabeMANummerDelete) {
+        MADelete.eingabeMANummerDelete = eingabeMANummerDelete;
+    }
+
+    private static String eingabeMANummerDelete;
 
     public static Scene createMADeleteScene(Stage stage){
 
         Label labelMADeleteScene= new Label("- MADelete- Scene - Mitarbeiter löschen");
 //MA-NummerNeu----------------------------------------------------------------------------------------------------------
-//        Label lMANummerNeu = new Label("Neue MA.-Nummer");
-//        TextField txfMANummerNeu = new TextField(eingabeMANummerNeu);
-//
-//        Button bMANummerNeu = new Button("MA-NummerNeuOK");
-//        Label lzeigeMANummerNeu = new Label();
-//        //bMANummer.setOnAction(e -> lzeigeMANummer.setText("Enthält keine Zahl!!!"+getEingabeMANummer()));
-//        //bMANummer.setOnAction(e -> lzeigeMANummer.setText("Enthält keine Zahl!!!"+txfMANummer.getText()));
-//        bMANummerNeu.setOnAction(e -> {
-//            lzeigeMANummerNeu.setText("JUNIT-Test: Enthält keine Zahl!!!"+txfMANummerNeu.getText());
-//            setEingabeMANummerNeu(txfMANummerNeu.getText());
-//            System.out.println(getEingabeMANummerNeu());
-//        });
+        Label lMANummerDelete = new Label("Delete MA.-Nummer");
+        TextField txfMANummerDelete = new TextField(eingabeMANummerDelete);
+
+        Button bMANummerDelete = new Button("MA-NummerDeleteOK");
+        Label lzeigeMANummerDelete = new Label();
+        //bMANummer.setOnAction(e -> lzeigeMANummer.setText("Enthält keine Zahl!!!"+getEingabeMANummer()));
+        bMANummerDelete.setOnAction(e -> {
+            lzeigeMANummerDelete.setText("JUNIT-Test: Enthält keine Zahl!!!"+txfMANummerDelete.getText());
+            setEingabeMANummerDelete(txfMANummerDelete.getText());
+            System.out.println(getEingabeMANummerDelete());
+        });
 
 //MADelete---------------------------------------------------------------------------------------------
         Button bMADelete = new Button("MA-Delete");
@@ -39,14 +47,16 @@ public class MADelete {
         Button bZurueckSpracheMADelete= new Button("Go to Sprache - Scene");
         bZurueckSpracheMADelete.setOnAction(e -> stage.setScene(Sprache.createSpracheScene(stage)));
 //LayoutZeugs-----------------------------------------------------------------------------------------------------------
-        VBox layoutVR= new VBox(1);
+        VBox layoutVD= new VBox(1);
 
+        HBox layoutH = new HBox(1);
 
+        layoutH.getChildren().addAll(lMANummerDelete,txfMANummerDelete,bMANummerDelete,lzeigeMANummerDelete);
 
-        layoutVR.getChildren().addAll(labelMADeleteScene,bMADelete,bZurueckSpracheMADelete,TaskLeistePane.getPane());
+        layoutVD.getChildren().addAll(labelMADeleteScene,layoutH,bMADelete,bZurueckSpracheMADelete,TaskLeistePane.getPane());
 
         //Scene sceneRegistrieren = new Scene(layoutVR);
-        Scene sceneMADelete = new Scene(new ScrollPane(layoutVR),700,400);//hat scrollpane drin
+        Scene sceneMADelete = new Scene(new ScrollPane(layoutVD),700,400);//hat scrollpane drin
         return sceneMADelete;
     }
 
