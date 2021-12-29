c9c52288f6053c024156229e01573b8296a9411d
John <flnc85@hotmail.de>
2021-12-29 22:29:49 +0100

    löschen MA funktioniert

--- src/main/java/GUI/MADelete.java
+++ src/main/java/GUI/MADelete.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import DatenBank.DatenBank;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -15,6 +16,8 @@ import javafx.stage.Stage;
  */
 public class MADelete {
 
+    static  DatenBank dBD = new DatenBank();
+
     public static String getEingabeMANummerDelete() {
         return eingabeMANummerDelete;
     }
@@ -24,25 +27,87 @@ public class MADelete {
     }
 
     private static String eingabeMANummerDelete;
+    private static String ausgabeGesamtMANummerDeleteFrageVonDB;
+
+    public static void setAusgabeSucheMANummerDeleteMitMANummerFrageDB(String ausgabeGesamtMANummerDeleteFrageVonDB) {
+        MADelete.ausgabeGesamtMANummerDeleteFrageVonDB = ausgabeGesamtMANummerDeleteFrageVonDB;
+    }
+    public static String getAusgabeSucheMANummerDeleteMitMANummerFrageDB() {
+        return ausgabeGesamtMANummerDeleteFrageVonDB;
+    }
+
 
     public static Scene createMADeleteScene(Stage stage){
 
         Label labelMADeleteScene= new Label("- MADelete- Scene - Mitarbeiter löschen");
 //MA-NummerNeu----------------------------------------------------------------------------------------------------------
         Label lMANummerDelete = new Label("Delete MA.-Nummer");
-        TextField txfMANummerDelete = new TextField(eingabeMANummerDelete);
+        TextField txfMANummerDelete = new TextField();
 
         Button bMANummerDelete = new Button("MA-NummerDeleteOK");
         Label lzeigeMANummerDelete = new Label();
         //bMANummer.setOnAction(e -> lzeigeMANummer.setText("Enthält keine Zahl!!!"+getEingabeMANummer()));
         bMANummerDelete.setOnAction(e -> {
-            lzeigeMANummerDelete.setText("JUNIT-Test: Enthält keine Zahl!!!"+txfMANummerDelete.getText());
-            setEingabeMANummerDelete(txfMANummerDelete.getText());
-            System.out.println(getEingabeMANummerDelete());
+            eingabeMANummerDelete = txfMANummerDelete.getText();
+
+            String strMANummerDeleteFrage = txfMANummerDelete.getText();
+            String ausgabeGesamtMANummerDeleteFrageVonDB = "SELECT * FROM userMubea WHERE MA_Nummer = '"+strMANummerDeleteFrage+"';";
+            setAusgabeSucheMANummerDeleteMitMANummerFrageDB(dBD.ausgebenGesamtDBRetourString(ausgabeGesamtMANummerDeleteFrageVonDB));
+            System.out.println("DBD AusgabeSucheMANummerDeleteMitMANummerFrageDB: " + getAusgabeSucheMANummerDeleteMitMANummerFrageDB());
+//eingabeMANummerNeu
+            if(eingabeMANummerDelete.length()==0){//wenn leer//eingabeVornameNeu.length()==0//strMANummerFrage==null
+                lzeigeMANummerDelete.setTextFill(Color.RED);
+                lzeigeMANummerDelete.setText("der zu löschende User muss einen Mitarbeiter- Nummer besitzen!");
+            }
+            else{//else if(eingabeMANummerNeu != null){
+                if(getAusgabeSucheMANummerDeleteMitMANummerFrageDB().isEmpty()){
+                    System.out.println("Mitarbeiter gibt es nicht");
+                    //ToDo Darf keine Leertaste haben und nur aus Zahlen bestehen!!!
+                    //lzeigeMANummerDelete.setText("JUNIT-Test: Enthält keine Zahl!!!" + txfMANummerDelete.getText());
+//                    lzeigeMANummerNeu.setTextFill(Color.GREEN);
+//                    setEingabeMANummerNeu(txfMANummerNeu.getText());
+//                    System.out.println(getEingabeMANummerNeu());
+                    lzeigeMANummerDelete.setTextFill(Color.RED);
+                    lzeigeMANummerDelete.setText("Der User existiert nicht!");
+                }
+                else {
+                    System.out.println("Mitarbeiter existiert");
+                    lzeigeMANummerDelete.setTextFill(Color.GREEN);
+                    lzeigeMANummerDelete.setText("Der User wurde gefunden");
+                    setEingabeMANummerDelete(txfMANummerDelete.getText());
+                }
+//                else if(!getAusgabeSucheMANummerMitMANummerFrageDB().isEmpty()&&eingabeMANummerNeu != null){
+//                lzeigeMANummerNeu.setTextFill(Color.RED);
+//                lzeigeMANummerNeu.setText("User muss einen Mitarbeiter- Nummer besitzen!");
+//                }
+            }
+
+
+
+
+//            lzeigeMANummerDelete.setText("JUNIT-Test: Enthält keine Zahl!!!"+txfMANummerDelete.getText());
+//            setEingabeMANummerDelete(txfMANummerDelete.getText());
+//            System.out.println(getEingabeMANummerDelete());
         });
 
 //MADelete---------------------------------------------------------------------------------------------
         Button bMADelete = new Button("MA-Delete");
+        Label lZeigeMADelete = new Label();
+        bMADelete.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                if(getEingabeMANummerDelete().length()==0){
+                    lZeigeMADelete.setText("Erst muss eine MA- Nummer eingegeben werden!");
+                    lZeigeMADelete.setTextFill(Color.RED);
+                }
+                else{
+                    String schreibeMADeleteInDB = "DELETE FROM userMubea WHERE MA_Nummer = '"+ getEingabeMANummerDelete()+"';";
+                    dBD.schreibeDB(schreibeMADeleteInDB);
+                    lZeigeMADelete.setText("Der User wurde erfolgreich gelöscht\nID geseht ez halt komisch us, aber mir egal");
+                    lZeigeMADelete.setTextFill(Color.GREEN);
+                }
+                setEingabeMANummerDelete("");
+            }
+        });
 //ZurückSprache---------------------------------------------------------------------------------------------------------
         Button bZurueckSpracheMADelete= new Button("Go to Sprache - Scene");
         bZurueckSpracheMADelete.setOnAction(e -> stage.setScene(Sprache.createSpracheScene(stage)));
@@ -50,10 +115,11 @@ public class MADelete {
         VBox layoutVD= new VBox(1);
 
         HBox layoutH = new HBox(1);
+        HBox layoutHDelete = new HBox(1);
 
         layoutH.getChildren().addAll(lMANummerDelete,txfMANummerDelete,bMANummerDelete,lzeigeMANummerDelete);
-
-        layoutVD.getChildren().addAll(labelMADeleteScene,layoutH,bMADelete,bZurueckSpracheMADelete,TaskLeistePane.getPane());
+        layoutHDelete.getChildren().addAll(bMADelete,lZeigeMADelete);
+        layoutVD.getChildren().addAll(labelMADeleteScene,layoutH,layoutHDelete,bZurueckSpracheMADelete,TaskLeistePane.getPane());
 
         //Scene sceneRegistrieren = new Scene(layoutVR);
         Scene sceneMADelete = new Scene(new ScrollPane(layoutVD),700,400);//hat scrollpane drin
